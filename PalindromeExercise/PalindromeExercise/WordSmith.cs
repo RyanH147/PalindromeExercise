ac9a29146fb71533670880a41f77b5f02f84f241
Ryan Hagood <ryanh147@gmail.com>
2020-10-22 13:02:51 -0500

    Committing PalindromeExercise Complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //reverse the word  //racecar ---> 7 ---> 6
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i]; //reversed = reversed + word[i]
+            }
+
+            //compare the original with the reversed
+            if(reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            //return whether they are the same or not
+        }
+    }
+}
